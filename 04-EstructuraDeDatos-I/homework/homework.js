3a46f15f10e226da3306ba7596b5006b959a69b5
alma <almacane36@gmail.com>
2023-12-11 13:40:50 -0300

    homework estructura de datos done

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -8,16 +8,27 @@ nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número 
 nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
 Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
 
-Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
+Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 
 
 
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
-function nFactorial(n) {}
+function nFactorial(n) {
+  if (n === 0 || n ===  1 )return 1 ;
+  return n * nFactorial(n - 1)
+  
+}
 
-function nFibonacci(n) {}
-
+function nFibonacci(n) {
+  if (n <=1) {
+    return n;
+  } 
+   else {
+    return nFibonacci(n - 1) + nFibonacci(n - 2);
+  }
+}
+//  console.log(nFibonacci(1))
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
   - enqueue: agrega un valor respetando el orden.
@@ -27,8 +38,43 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {}
+class Queue {
+  constructor()
+  {
+    this._queue_ = []
+    
+  }
 
+  enqueue(info)
+  {
+    this._queue_.push(info)
+    
+  }
+  dequeue(){
+    
+     return this._queue_.shift()
+  }
+  size(){
+    
+
+    return this._queue_.length
+  }
+}
+const miqueue = new Queue();
+const q2 = new Queue();
+// miqueue.enqueue('first');
+//     miqueue.enqueue('second');
+//     miqueue.enqueue('third');
+//     miqueue.dequeue();
+//     console.log(miqueue);
+//     miqueue.dequeue();
+//     console.log(miqueue);
+//     miqueue.dequeue();
+//     console.log(miqueue);
+// console.log(miqueue.size())
+// console.log(miqueue.dequeue());
+// console.log(miqueue);
+// console.log(miqueue.size());
 /*⚠️ No modificar nada debajo de esta línea ⚠️*/
 module.exports = {
    Queue,
