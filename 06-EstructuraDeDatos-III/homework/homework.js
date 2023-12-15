18d541c35b7da2d0d6a85d68e2addf8cbb76472c
alma <almacane36@gmail.com>
2023-12-15 01:08:06 -0300

    edd III done

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -9,56 +9,88 @@
   - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
-class BinarySearchTree {
-   constructor(value)
-   {
-      this.value = value;
-      this.left = null;
-      this.right = null;
-   }
-
-   insert(valor)
-   {
-      let newBST= new BinarySearchTree(valor)
-      if(!this.value){this value = valor}
-      if (valor< this.value)
-      {
-         if(!this.left)
-         {this.left= newBST
-         }
-         else
-         {this.left.insert(valor) }
-      }
-      else if(valor> this.value)
-      {
-         if(!this.right)
-         {this.right= newBST
-         }
-         else
-         {this.right.insert(valor) }
-      }
-     
-   }
-
-contains(value)
-{if (!this.value) return false;
-   else if(this.value === value)
-   {return true}
-      else if(this.left.contains(value)){return true
-     }
-     else if(this.right.contains(value)){return true}
-     else return false;
-
-}
-   size(){
-     
-   }
+function BinarySearchTree(value) {
+   this.value = value;
+   this.left = null;
+   this.right = null;
 }
 
-let newArbol = new BinarySearchTree;
-newArbol.insert(10)
-newArbol.insert(18)
-console.log(newArbol);
+BinarySearchTree.prototype.size = function(){
+  
+   if(!this.left && !this.right) return 1
+
+   if(!this.right) return 1 + this.left.size()
+   if(!this.left) return 1 + this.right.size()
+   return 1 + this.left.size() + this.right.size()
+}
+
+BinarySearchTree.prototype.insert = function(value){
+   if(value < this.value){
+      if(this.left === null){
+         const myTree = new BinarySearchTree(value)
+         this.left = myTree;
+      }else{
+         this.left.insert(value)
+      }
+   }else if(value >= this.value) {
+      if(!this.right){
+         const myTree = new BinarySearchTree(value)
+         this.right = myTree;
+      }else{
+         this.right.insert(value)
+      }
+   }
+   BinarySearchTree.prototype.depthFirstForEach = function(cb, recorrido){
+      if(recorrido === 'in-order' || !recorrido){
+         // ? nodo izquierdo - nodo padre - nodo derecho
+        this.left && this.left.depthFirstForEach(cb, recorrido)
+         cb(this.value)
+         this.right && this.right.depthFirstForEach(cb, recorrido)
+      }
+      if(recorrido === 'pre-order'){
+      
+         cb(this.value)
+         this.left && this.left.depthFirstForEach(cb, recorrido)
+         this.right && this.right.depthFirstForEach(cb, recorrido)
+      }
+   
+      if(recorrido === 'post-order'){
+         
+         this.left && this.left.depthFirstForEach(cb,recorrido)
+         this.right && this.right.depthFirstForEach(cb, recorrido)
+         cb(this.value)
+      }
+   }
+   
+   BinarySearchTree.prototype.breadthFirstForEach = function(cb, arr = []){
+   
+      cb(this.value) 
+      this.left && arr.push(this.left)
+      this.right && arr.push(this.right)
+      // console.log(arr);
+   
+      arr.length && arr.shift().breadthFirstForEach(cb, arr)
+   }
+   
+   
+   BinarySearchTree.prototype.contains = function(value){
+      if(value === this.value) return true;
+      if(value < this.value){
+         if(!this.left) return false;
+         return this.left.contains(value)
+      }
+      if(value > this.value){
+         if(!this.right) return false;
+         return this.right.contains(value)
+      }
+   }
+   
+   
+}
+// let newArbol = new BinarySearchTree;
+// newArbol.insert(10)
+// newArbol.insert(18)
+// console.log(newArbol);
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
